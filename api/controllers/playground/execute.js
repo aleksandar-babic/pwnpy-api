dc610e8236e941a291a4a0f11448fd16ee5c3a6f
Aleksandar Babic <alekski64@icloud.com>
2018-09-27 22:45:54 +0200

    minor change to experience playground logic

--- api/controllers/playground/execute.js
+++ api/controllers/playground/execute.js
@@ -28,18 +28,21 @@ module.exports = {
       const source = this.req.file('source')._files[0];
       const res = await sails.helpers.microservice.execute(source);
 
+      let updatedUser;
       if (res.exitCode === 0) {
         const user = await User.findOne({
           id: this.req.user.data.id
         });
 
-        await User.update({
+        updatedUser = await User.update({
           id: user.id
         }, {
           experience: user.experience + 50
-        });
+        }).fetch();
       }
-      return exits.success(res);
+      return exits.success({ ...res,
+        user: updatedUser ? updatedUser[0] : null
+      });
     } catch (err) {
       return exits.error(err.message)
     }
