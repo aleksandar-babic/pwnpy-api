925d4ec59a1f35f6f8435af7ebf978d424e23430
Aleksandar Babic <alekski64@icloud.com>
2018-09-27 22:33:46 +0200

    Added experience for playground

--- api/controllers/playground/execute.js
+++ api/controllers/playground/execute.js
@@ -27,6 +27,18 @@ module.exports = {
       }
       const source = this.req.file('source')._files[0];
       const res = await sails.helpers.microservice.execute(source);
+
+      if (res.exitCode === 0) {
+        const user = await User.findOne({
+          id: this.req.user.data.id
+        });
+
+        await User.update({
+          id: user.id
+        }, {
+          experience: user.experience + 50
+        });
+      }
       return exits.success(res);
     } catch (err) {
       return exits.error(err.message)
